9585d63159660f087d74d41aee3265bfdc156d46
ndongogueye <bayeg2003@gmail.com>
2023-07-17 09:01:59 +0200

    #3

--- assets/file.js
+++ assets/file.js
@@ -5,3 +5,22 @@ targetDate.setMinutes(30);
 targetDate.setSeconds(0);
 
 
+function updateCountdown() {
+    let now = new Date();
+    let timeDifference = targetDate - now;
+  
+    if (timeDifference <= 0) {
+      clearInterval(countdownInterval);
+      console.log("Conto alla rovescia finito");
+      return;
+    }
+  
+   
+    let ore = Math.floor(timeDifference / (1000 * 60 * 60));
+    let minuti = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    let secondi = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  
+    console.log("Conto alla rovescia: " + ore + "h " + minuti + "m " + secondi + "s");
+  }
+  
+  let countdownInterval = setInterval(updateCountdown, 1000);
